3d781641a7411fa1e795a73d00f3e567ae839a82
jxshi03 <jxshi03@users.noreply.github.com>
2026-05-27 12:06:07 +0800

    init

--- static/js/index.js
+++ static/js/index.js
@@ -10,4 +10,38 @@ $(document).ready(function() {
     $('.navbar-burger').removeClass('is-active');
     $('.navbar-menu').removeClass('is-active');
   });
+
+  $('.rollout-row').each(function() {
+    var $row = $(this);
+    var $scrollbar = $('<div class="rollout-scrollbar"><div class="rollout-scrollbar-inner"></div></div>');
+    $row.after($scrollbar);
+
+    function syncWidth() {
+      $scrollbar.find('.rollout-scrollbar-inner').width($row.get(0).scrollWidth);
+    }
+
+    var isSyncingRow = false;
+    var isSyncingBar = false;
+
+    $row.on('scroll', function() {
+      if (isSyncingBar) {
+        isSyncingBar = false;
+        return;
+      }
+      isSyncingRow = true;
+      $scrollbar.scrollLeft($row.scrollLeft());
+    });
+
+    $scrollbar.on('scroll', function() {
+      if (isSyncingRow) {
+        isSyncingRow = false;
+        return;
+      }
+      isSyncingBar = true;
+      $row.scrollLeft($scrollbar.scrollLeft());
+    });
+
+    syncWidth();
+    $(window).on('resize', syncWidth);
+  });
 });
